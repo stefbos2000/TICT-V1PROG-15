af472a53a72a540b3f50fb1772a52f977d543d7f
Stef Bos <stefbos2000@gmail.com>
2017-09-10 20:43:30 +0200

    De opdrachten van practice exercises 2_1 t/m 2_4

--- pe2/pe2_4.py
+++ pe2/pe2_4.py
@@ -1,5 +1,4 @@
 uurlooon = input('Wat verdien je per uur: ')
 aantal_uur = input('Hoeveel uur heb je gewerkt: ')
-geld = uurlooon * aantal_uur
-print(geld)
+geld = int(uurlooon) * int(aantal_uur)
 print ((str(uurlooon)) + ' uur werken levert ' + (str(geld)) + ' euro op')
